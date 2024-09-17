fdf4f947c5d8f4492ab470359e8329415c62b835
Bala Murugan <balamurugand@hexamapsolutions.com>
2024-09-17 13:40:06 +0530

    Modularisation and code execution

--- 5.currency_converter.py
+++ 5.currency_converter.py
@@ -3,12 +3,7 @@
 #calculation
 
 
-currency_list = ('usd','eur','cad')
 
-exchage_rates = { 'usd': {'eur':0.85 ,'cad': 1.13},
-                   'eur':{'usd': 1.18, 'cad': 1.47},
-                    'cad': {'usd': 0.80, 'eur': 0.68} 
-                    }
 
 def get_amount():
     while True:
@@ -23,6 +18,7 @@ def get_amount():
             print("invalid_value")
 
 def check_currency(label):
+    currency_list = ('usd','eur','cad')
     while True:
         currency_selected = input(f'{label} (USD/CAD/EUR): ').lower()
         if (currency_selected not in currency_list):
@@ -32,23 +28,28 @@ def check_currency(label):
 
 
 def convert( amount,source_currency,target_currency):
+        exchage_rates = { 'usd': {'eur':0.85 ,'cad': 1.13},
+                    'eur':{'usd': 1.18, 'cad': 1.47},
+                        'cad': {'usd': 0.80, 'eur': 0.68} 
+                        }
         if (source_currency == target_currency):
             return amount
         return amount * exchage_rates[source_currency][target_currency]
 
+def main():
+    amount = get_amount()
+    source_currency = check_currency("source_currency")
+    target_currency = check_currency("target_currency")
+    converted_amount = convert(amount,
+                            source_currency=source_currency, 
+                            target_currency= target_currency)
 
-amount = get_amount()
-source_currency = check_currency("source_currency")
-target_currency = check_currency("target_currency")
-converted_amount = convert(amount,
-                        source_currency=source_currency, 
-                        target_currency= target_currency)
-
-print(f'{amount} {source_currency} is equal to {converted_amount:.2f} of {target_currency}')
+    print(f'{amount} {source_currency} is equal to {converted_amount:.2f} of {target_currency}')
 
 
 
-       
+if __name__ == "__main__":
+    main()   
 
 
 
