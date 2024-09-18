767d223edc391fa04b6be41daeb4bee2da80dd9e
Bala Murugan <balamurugand@hexamapsolutions.com>
2024-09-18 13:40:48 +0530

    Final_changes after Modularisation Lesson Learnt is keeping the same function under single function

--- 6.quiz_game.py
+++ 6.quiz_game.py
@@ -1,47 +1,44 @@
-import sys
 from termcolor import colored, cprint
 import random 
 
-quiz = [
-    {"question" : 'What is the Captial of France?',
-     'options' : ['A.Berlin','B.Madrid', 'C.Paris', 'D.Rome'],
-     'Answer': 'C'},
-     {"question" : 'Which planet is known as the red planet?',
-     'options' : ['A.Earth','B.Mars', 'C.Jupiter', 'D.Saturn'],
-     'Answer': 'B'},
-     {"question" : 'What is the largest ocean on the Earth?',
-     'options' : ['A.Atlantic', 'B.Indian', 'C.Arctic','D.Pacific'],
-     'Answer': 'D'}
-]
+QUESTION = 'question'
+ANSWER = 'Answer'
+OPTION ='options'
 
-score = 0
-random.shuffle(quiz)
-for index , items in enumerate(quiz,1):
-    print(f'Question {index}: {items['question']}')
-    for option in items['options']:
-        print(option)
-    answer = input ('Your Answer: ').upper().strip()
-    if answer == items['Answer']:
-        score +=1
-        cprint("Correct", "green")
-    else:
-        cprint(f'Wrong ,The Correct answer is {items["Answer"]}',"red")
+def ask_question(index, question, option):
+        print(f'Question {index}: {question}')
+        for option in option:
+            print(option)
+        return input ('Your ANSWER: ').upper().strip()
 
-print(f'Your Final Score is {score} out of {len(quiz)}')
+def run_quiz(quiz):
+    score = 0
+    random.shuffle(quiz)
 
+    for index, item in enumerate(quiz,1):
+        answer = ask_question(index,item[QUESTION],item[OPTION])
 
+        if answer == item[ANSWER]:
+            score +=1
+            cprint("Correct", "green")
+        else:
+            cprint(f'Wrong ,The Correct answer is {items[ANSWER]}',"red")
 
+    print(f'Your Final Score is {score} out of {len(quiz)}')
 
+def main():
+    quiz = [
+        {QUESTION : 'What is the Captial of France?',
+        OPTION : ['A.Berlin','B.Madrid', 'C.Paris', 'D.Rome'],
+        ANSWER: 'C'},
+        {QUESTION : 'Which planet is known as the red planet?',
+        OPTION : ['A.Earth','B.Mars', 'C.Jupiter', 'D.Saturn'],
+        ANSWER: 'B'},
+        {QUESTION : 'What is the largest ocean on the Earth?',
+        OPTION : ['A.Atlantic', 'B.Indian', 'C.Arctic','D.Pacific'],
+        ANSWER: 'D'}
+    ]
+    run_quiz(quiz)
 
-
-# second_answer = input ("Which planet is known as the red planet \n A.Earth \n B.Mars \n C.Jupiter \n D.Saturn \n Your answer: ")
-# third_answer = input ("What is the largest ocean on the Earth \n A.Atlantic \n B.Indian \n C.Arcitc \n D.Pacific \n Your answer: ")
-
-
-# go to the next question
-# display the correct_answer
-# Display the score
-
-
-
-# cprint("Hello, World!", "green")
+if __name__ == '__main__':
+     main()
