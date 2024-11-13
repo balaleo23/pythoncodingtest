fa3c81c2fefa169f6def457fb51fd9acf239b104
Bala Murugan <balamurugand@hexamapsolutions.com>
2024-11-13 14:58:40 +0530

    need to fix the failure scenario

---
+++ 13.1passsword_generator.py
@@ -0,0 +1,36 @@
+# generate string
+#ask for length to generate
+# need to include the uppercase letter
+#include  the numbers
+#include special character
+import random
+import string
+
+
+def generateword(length,include_uppercase, include_numbers, include_special):
+            format = "string.ascii_lowercase "
+            if(include_uppercase == 'y'):
+                format = format + "+ string.ascii_uppercase "
+            if(include_numbers=="y"):
+                  format = format+ "+ string.digits"
+            if(include_special == 'y'):
+                  format = format + '+ string.punctuation'
+            
+            print(format)
+            res = ''.join(random.choices(format, k= length))
+            # res = ''.join(random.choices(string.ascii_lowercase  , k=length))
+            return res
+    
+          
+
+def main ():
+
+    length =  int(input("Enter Password Length: "))
+    include_uppercase =  input("Include upperCase letter?(y/n): ")
+    include_numbers = input("Include numbers?(y/n): ")
+    include_special = input("Include Special Characters? (y/n): ")
+
+    word_generated = generateword(length, include_uppercase, include_numbers, include_special)
+    print(word_generated)
+if __name__ == '__main__':
+    main()
